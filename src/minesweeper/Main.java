29f4653afdf39b44e976a874857c6cc587a4fa61
mooreEffort <alexmaksimets@gmail.com>
2023-01-23 20:01:00 +1000

    starting from scratch

--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -1,17 +1,10 @@
 package minesweeper;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			Minesweeper game = new Minesweeper();
-			game.startGame();
-		}
-				
-		
-}
 
+	}
+
+}
